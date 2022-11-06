c8445bc45faf75b7dc285363acb773a5aaedfdf3
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2022-11-06 20:08:16 +0700

    vinh

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -1,12 +1,27 @@
 import problem
+from bitarray import bitarray as ba
 
 class geneticAl:
     def __init__(self):
         pass
 
-    def __call__(self, problem:problem.knapsack):
+    def initialize(self, length:int, init_items:int = 5) -> None:
+        pass
+
+    def extract_neccessary_data(self, problem:problem.knapsack):
+        '''
+        return length_of_each_item, list_of_begin_index
+        '''
+        pass
+
+    def __call__(self, problem:problem.knapsack) -> problem.result:
         pass
 
 
 if __name__ == '__main__':
+    a = ba(
+        '110'
+    )
+
+    print(a)
     pass
