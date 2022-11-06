970ed0436c4a69a25190f68dcb782578677cff8d
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2022-11-06 18:54:27 +0700

    init

---
+++ geneticAlgorithm.py
@@ -0,0 +1,12 @@
+import problem
+
+class geneticAl:
+    def __init__(self):
+        pass
+
+    def __call__(self, problem:problem.knapsack):
+        pass
+
+
+if __name__ == '__main__':
+    pass
--- problem.py
+++ problem.py
@@ -1,6 +1,6 @@
 import json
 
-class knapsack:
+class knapsack(object):
     '''
     data file structure example:
     {
@@ -17,6 +17,9 @@ class knapsack:
     - self.w: weight        (float list)
     - self.v: value         (float list)
     - self.q: quatity       (int list)
+
+    NOTE: len(class) = len(value) = len(weight) = len(quatity)
+        - item index n will be (class[n], value[n], weight[n], quatity[n])
     '''
     def __init__(self, path='./data.json'):
         with open(path) as rf:
@@ -26,5 +29,21 @@ class knapsack:
             self.v = data['value']
             self.w = data['weight']
             self.q = data['quatity']
+
+    def __getitem__(self, key):
+        '''item index n will be (class[n], value[n], weight[n], quatity[n])'''
+        return (self.c[key], self.v[key], self.w[key], self.q[key])
+
+    def len(self):
+        '''return number of items'''
+        return len(self.c)
         
     
+class result:
+    '''
+    the quatity of each item that we take:
+    for ex:
+        self.res = [1,2,1] means: take 1 from item 1, 2 from item 2, 1 from item 3
+    '''
+    def __init__(self, res:list):
+        self.res = res
