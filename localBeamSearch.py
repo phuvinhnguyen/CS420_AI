dbd21c78721c65d315ca658228a5a4ddd33a799e
NhanHcmus20 <81221996+NhanHcmus20@users.noreply.github.com>
2022-11-20 18:21:49 +0700

    bug

--- localBeamSearch.py
+++ localBeamSearch.py
@@ -6,7 +6,7 @@ from random import randint
 weights = [85, 26, 48, 21, 22, 95, 43, 45, 55, 52]
 values = [79, 32, 47, 18, 26, 85, 33, 40, 45, 59]
 class_=[1, 1, 2, 1, 2, 1, 1, 2, 2, 2]
-
+MaxWeight = 101
 
 # initial state will be [1,1,1,1,1,..,1] -> each item will have 1 so the total weight will be over the max weight can have
 # we reduce the number of item one by one (these are successors of initial state) randomly with heuristic : remove the lowest value first
@@ -17,13 +17,20 @@ def _init_state(weights):
         first_state.append(1)
     return first_state
 
-def localBeam(weights):
-    sol = _init_state(weights)
+def localBeam(weights,k):
+    #first state : [1,1,..,1]
+    f = _init_state(weights)
+    #list of acceptable state
+    a = []
     #reduce item in the init state in the descending order of value (heuristic)
+    while len(a) < k:
+        r = randint(0,len(f)-1)
+        if (f[r] == 1):
+            f[r] = 0
+            if(calWeight(f,weights) < MaxWeight): a.append(f)
+
     
-    
-    
-    return sol
+    return a
 
 # get value to compare the best value later
 def calValue(state,values):
@@ -38,4 +45,13 @@ def calWeight(state,weights):
     for i in range(0,len(state)):
         if(state[i] == 1): cur_weight += weights[i]
     return cur_weight
-    
+
+#check if can add more value if the current state weight is lower than max weight
+def checkAdd(state):
+    for s in state:
+        if s == 0:
+            s = 1
+            if (calWeight(state,weights) < MaxWeight): break
+            else: s = 0
+
+print(localBeam(weights,5))
