e946c2ed8103b8d616eddb3559eded6345cb1457
“phuvinhnguyen” <phuvinhnguyen812002@gmail.com>
2022-11-20 12:47:26 +0700

    brute

--- BruteForceSearch.py
+++ BruteForceSearch.py
@@ -1,26 +1,43 @@
 from problem import knapsack, split_data
 
 
+def bruteForce(maxWeight,C,weight,value,c):
+    #initial state 0x00...00
+    i=0
+    #max score
+    maxS = 0
+    #result
+    result = ''
+    #iterate though all solutions
+    while i != (1 << len(value)):
+        num = bin(i)[2:]
+        solution = '0'*(len(value) - len(num)) + num
+        _v,_w,_c = 0,0,[j for j in range(1,C+1)]
 
-def BruteForceSearch(W, w, v, n):
+        for it in range(len(solution)):
+            if '1' == solution[it]:
+                _v += int(value[int(it)])
+                _w += int(weight[int(it)])
+                if int(c[int(it)]) in _c:
+                    _c.remove(int(c[int(it)]))
+        
+        if _w > maxWeight or len(_c) > 0:
+            _v = 0
+        
+        if _v > maxS:
+            maxS = _v
+            result = solution
+        
+        i += 1
 
-    if n == 0 or W == 0:
-        return 0
- 
-    if (w[n-1] > W):
-        return BruteForceSearch(W, w, v, n-1)
-    else:
-        return max(v[n-1] + BruteForceSearch(W-w[n-1], w, v, n-1), BruteForceSearch(W, w, v, n-1))
-    
+    return maxS, str([int(i) for i in result])[1:-1]
 
-    
- 
- 
-#Driver Code
-v = [85, 100, 120]
-w = [10, 20, 30]
-W = 50
-n = len(v)
-print (BruteForceSearch(W, w, v, n))
- 
-# This code is contributed by Nikhil Kumar Singh
+if __name__ == '__main__':
+    data = split_data('input/small_dataset.txt')
+
+    with open('output/OUTPUT_'+str(data.len())+'.txt', 'w') as wf:
+        for i in range(data.len()):
+            prob = knapsack(data=data[i])
+            maxV, sol = bruteForce(prob.getMaxWeight(), prob.getClassNum(), prob.w, prob.v, prob.c,)
+            print(maxV)
+            print(sol)
