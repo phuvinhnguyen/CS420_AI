e8076303011d5fc126fdabad7f888b3b4f6953fe
NhanHcmus20 <81221996+NhanHcmus20@users.noreply.github.com>
2022-11-20 23:36:10 +0700

    local

--- localBeamSearch.py
+++ localBeamSearch.py
@@ -3,8 +3,8 @@ import numpy as np
 from random import randint
 
 
-MaxWeight = 5
-k = 5
+
+k = 3
 # initial state will be [1,1,1,1,1,..,1] -> each item will have 1 so the total weight will be over the max weight can have
 # we reduce the number of item one by one (these are successors of initial state) randomly with heuristic : remove the lowest value first
 # after removed item until the weight is lower than max weight, we will check if we can add any small-weight item
@@ -24,7 +24,7 @@ def localBeam(init, weights,k):
         x[i] = 0
         list_of_generate.append(x)
 
-    keep_generate = top(list_of_generate)
+    keep_generate = top(list_of_generate,k)
 
 
     bestV = 0
@@ -44,7 +44,7 @@ def valid(sol:list):
     #return true if sol is valid
     total_weight = 0
     for i in range(0,len(sol)-1):
-        if(sol[i] == 1): total_weight += weights[i]
+        if(sol[i] == 1): total_weight += int(weights[i])
     
     
     if(total_weight <= MaxWeight): return True
@@ -55,7 +55,7 @@ def value(sol:list):
     #return value of sol
     total_value = 0
     for i in range(0,len(sol)-1):
-        if(sol[i] == 1): total_value += values[i]
+        if(sol[i] == 1): total_value += int(values[i])
     return total_value
 
 def top(sol_list:list, t = 5):
@@ -63,7 +63,16 @@ def top(sol_list:list, t = 5):
     list_value = []
     for i in sol_list:
         list_value.append(value(i))
-    return []
+    tmp = list_value.copy()
+    tmp.sort()
+    tmp.reverse()
+    index = []
+    for i in range(0,t-1):
+         index.append(list_value.index(tmp[i]))
+    res = []
+    for i in range(0,t-1):
+        res.append(sol_list[index[i]])
+    return res
 
 # def score(sol):
 #     #calculate score to use in select top in top function
@@ -78,14 +87,18 @@ def top(sol_list:list, t = 5):
 if __name__ == '__main__':
     data = split_data('input/small_dataset.txt')
 
-    # with open('output/OUTPUT_'+str(data.len())+'.txt', 'w') as wf:
-    for i in range(data.len()):
-        prob = knapsack(data=data[i])
-        values = prob.v
-        weights = prob.w
-        init = []
-        for i in weights:
-           init.append(1) 
-        bestV, sol = localBeam(init,weights,k)
-        print(bestV)
-        print(sol)
+    with open('output/OUTPUT_'+str(data.len())+'.txt', 'w') as wf:
+        for i in range(data.len()):
+            prob = knapsack(data=data[i])
+            values = prob.v
+            weights = prob.w
+            MaxWeight = int(prob.getMaxWeight())
+            init = []
+            for i in range(0,len(weights)-1):
+                init.append(1)
+            print(init)
+            print(type(init))
+            bestV, sol = localBeam(init,weights,k)
+            print(bestV)
+            print(sol)
+   
