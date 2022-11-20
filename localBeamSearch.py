aea0080eb706b6c0c7d78d38ef2c557aa8972d9d
NhanHcmus20 <81221996+NhanHcmus20@users.noreply.github.com>
2022-11-20 20:44:45 +0700

    localBeam

--- localBeamSearch.py
+++ localBeamSearch.py
@@ -11,47 +11,69 @@ MaxWeight = 101
 # initial state will be [1,1,1,1,1,..,1] -> each item will have 1 so the total weight will be over the max weight can have
 # we reduce the number of item one by one (these are successors of initial state) randomly with heuristic : remove the lowest value first
 # after removed item until the weight is lower than max weight, we will check if we can add any small-weight item
-def _init_state(weights):
-    first_state = []
-    for i in range(0,len(weights)-1):
-        first_state.append(1)
-    return first_state
-
-def localBeam(weights,k):
-    #first state : [1,1,..,1]
-    f = _init_state(weights)
-    #list of acceptable state
-    a = []
-    #reduce item in the init state in the descending order of value (heuristic)
-    while len(a) < k:
-        r = randint(0,len(f)-1)
-        if (f[r] == 1):
-            f[r] = 0
-            if(calWeight(f,weights) < MaxWeight): a.append(f)
-
     
-    return a
 
-# get value to compare the best value later
-def calValue(state,values):
-    total = 0
-    for i in range(0,len(state)):
-        if(state[i] == 1): total += values[i]
-    return total
+def localBeam(init, weights,k):
+    if 1 not in init:
+        return 0, None
 
-# get weight to check current weight is lower than max weight
-def calWeight(state,weights):
-    cur_weight = 0
-    for i in range(0,len(state)):
-        if(state[i] == 1): cur_weight += weights[i]
-    return cur_weight
+    #list of acceptable state
+    list_of_generate = []
+    #reduce item in the init state in the descending order of value (heuristic)
+    for i in range(len(init)):
+        if i == 0:
+            continue
+        x = init
+        x[i] = 0
+        list_of_generate.append(x)
 
-#check if can add more value if the current state weight is lower than max weight
-def checkAdd(state):
-    for s in state:
-        if s == 0:
-            s = 1
-            if (calWeight(state,weights) < MaxWeight): break
-            else: s = 0
+    keep_generate = top(list_of_generate)
 
-print(localBeam(weights,5))
+
+    bestV = 0
+    result = None
+    if valid(init) == True:
+        bestV = value(init)
+        result = init
+    for i in keep_generate:
+        v, sol = localBeam(i, weights, k)
+        if v >= bestV:
+            bestV = v
+            result = sol
+    
+    return bestV, result
+
+def valid(sol:list):
+    #return true if sol is valid
+    total_weight = 0
+    for i in range(0,len(sol)-1):
+        if(sol[i] == 1): total_weight += weights[i]
+    
+    
+    if(total_weight <= MaxWeight): return True
+    else: return False
+    
+
+def value(sol:list):
+    #return value of sol
+    total_value = 0
+    for i in range(0,len(sol)-1):
+        if(sol[i] == 1): total_value += values[i]
+    return total_value
+
+def top(sol_list:list, t = 5):
+    #return top t solutions of the sol list(best solutions)
+    list_value = []
+    for i in sol_list:
+        list_value.append(value(i))
+    return []
+
+# def score(sol):
+#     #calculate score to use in select top in top function
+#     return 0
+
+
+# print(localBeam(weights,5))
+# print(_init_state(weights))
+# print(calWeight(_init_state(weights),weights))
+print(localBeam([1,1,1,1,1,1,1,1,1,1],weights,5))
