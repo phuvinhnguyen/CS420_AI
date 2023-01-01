26a6549022000b9a695e8fd03e96f0c28cf982b9
David <anhlq28102002@gmail.com>
2023-01-01 15:47:21 +0700

    u

--- TreasureIsland/main.py
+++ TreasureIsland/main.py
@@ -9,20 +9,19 @@ import imageio.v3 as iio
 import copy
 
 
-# def displayText(map):
-#     for i in range(len(data)):
-#         for j in range(len(data[0])):
-#             texts[i][j].remove()
-#             texts[i][j] = ax.text(j, i, map[i][j],ha="center", va="center", color="w")
+def displayText(map):
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            texts[i][j].remove()
+            texts[i][j] = ax.text(j, i, map[i][j],ha="center", va="center", color="w")
             
-#     # Used to return the plot as an image rray
-#     fig.canvas.draw()       # draw the canvas, cache the renderer
-#     image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
-#     image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-#     return image
+    # Used to return the plot as an image rray
+    fig.canvas.draw()       # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    return image
 
-def updateMap(x,y,z,t,k,map):
-    # map[x][y].translate({ord(k): None})
+def updateMap(z,t,k,map):
     map[z][t] += k
     
 def get_init_place(mmap:nmap):
@@ -34,14 +33,14 @@ def get_init_place(mmap:nmap):
             return [pos[0][sel],pos[1][sel]]
 
 if __name__ == '__main__':
-    mmap = nmap('input/map64_1.txt')
+    mmap = nmap("input/map64_1.txt")
     pir = pirate.pirate(mmap)
     maps = [] #lưu giá trị các bước di chuyễn
     agent_views = []
     init_place = get_init_place(mmap) #generate initial place
     result = None
     rpp = mmap.turnnumber_pp
-
+    
     agent = agentkm(init_place, mmap)
 
     while True:
@@ -66,15 +65,15 @@ if __name__ == '__main__':
         agent.step(input)
         agent_view = copy.deepcopy(agent.mask.mask)
 
-        print(input)
-        print(agent_view)
+        #print(input)
+        #print(agent_view)
 
         agent_views.append(agent_view)
         agent_pos = agent.report()
         pirate_pos = pir.report()
-        map = copy.deepcopy(mmap.mmap)
-        updateMap(agent_prev_pos[0],agent_prev_pos[1],agent_pos[0],agent_pos[1],"A",map)
-        updateMap(pirate_prev_pos[0],pirate_prev_pos[1],pirate_pos[0],pirate_pos[1],"Pr",map)
+        map=copy.deepcopy(mmap.mmap)
+        updateMap(agent_pos[0],agent_pos[1],"A",map)
+        updateMap(pirate_pos[0],pirate_pos[1],"Pr",map)
         maps.append(map)
     
     with open('output/o.txt', '+w') as wf:
@@ -86,27 +85,31 @@ if __name__ == '__main__':
                 wf.write(str(line)+'\n')
             wf.write('-----------------------------------\n')
         wf.write(result)
+
+    #print(maps[0])
     #show result
     # Generating data for the heat map
-    # data = []
-    # for i in range(len(mmap.mmap)):
-    #     d=[]
-    #     for j in range(len(mmap.mmap[0])):
-    #         t=mmap.mmap[i][j]
-    #         d.append(int(t.translate({ord(k): None for k in 'MPT'})))
-    #     data.append(d)
+    data = []
+    for i in range(len(map)):
+        d=[]
+        for j in range(len(map[0])):
+             t=mmap.mmap[i][j]
+             d.append(int(t.translate({ord(k): None for k in 'MPT'})))
+        data.append(d)
+    print(data)
                   
-    # fig = plt.figure()
-    # # Function to show the heat map
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(data)
-
-    # texts=[]
-    # for i in range(len(data)):
-    #     text=[]
-    #     for j in range(len(data[0])):
-    #         text.append(ax.text(j, i, "0",ha="center", va="center", color="w"))
-    #     texts.append(text)
-    # #ims=[]
-    # kwargs_write = {'fps':1.0, 'quantizer':'nq'}
-    # iio.imwrite("./powers.gif", [displayText(i) for i in maps], duration=1000)
+    fig = plt.figure()
+    # Function to show the heat map
+    fig, ax = plt.subplots()
+    im = ax.imshow(data)
+    #fig.tight_layout()
+    #plt.show()
+    texts=[]
+    for i in range(len(data)):
+        text=[]
+        for j in range(len(data[0])):
+           text.append(ax.text(j, i, "0",ha="center", va="center", color="w"))
+        texts.append(text)
+    ims=[]
+    kwargs_write = {'fps':1.0, 'quantizer':'nq'}
+    iio.imwrite("./move.gif", [displayText(i) for i in maps], duration=1000)
